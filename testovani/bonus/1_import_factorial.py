43553d3dd65bfe8878b0abecf15ddb52ef8e77d0
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2017-10-30 22:41:16 +0100

    Added bonus task

---
+++ testovani/bonus/1_import_factorial.py
@@ -0,0 +1,7 @@
+#Ukol: Z modulu math importuj funkci factorial
+#Zeptej se uzivatele na cislo, a vypocuitej faktorial tohoto cisla.
+#Vice o funkcich z math: https://docs.python.org/2/library/math.html
+
+#Rozsirujici zadani:
+# - Osetri vstup tak, aby uzivatel mohl zadat pouze cele cislo
+# - Osetri vstup tak aby uzivatel nemohl zadat zaporne cislo

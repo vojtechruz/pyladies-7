710e26590e07f1879a87d7d8494d61587b3cfd53
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2017-10-30 22:46:13 +0100

    Added bonus task

---
+++ testovani/bonus/2_vyhazovani vyjimek.py
@@ -0,0 +1,3 @@
+#Ukol: At uzivatel zada desetinne cislo v rozmezi 50.0 a 150.0
+# - pokud neni zadano desetinne cislo, ptej se stale znovu
+# - pokud cislo je desetinne, ale neni v tomto rozmezi vyhod ValueError vyjimku

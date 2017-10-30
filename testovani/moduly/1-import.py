cbc6d17d7a9f0d03f2a0384505c000e7e81f2cdb
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2017-10-30 20:47:16 +0100

    Code examples for modules

---
+++ testovani/moduly/1-import.py
@@ -0,0 +1,5 @@
+# Import funkce sqrt (Square root - odmocnina) z modulu math
+from math import sqrt, pi
+
+print("Odmocnina z cisla 196 je {}".format(sqrt(196)))
+print("PI je {}".format(pi))
---
+++ testovani/moduly/2-import-module.py
@@ -0,0 +1,5 @@
+# Import celeho modulu math
+import math
+
+print("Odmocnina z cisla 196 je {}".format(math.sqrt(196)))
+print("PI je {}".format(math.pi))

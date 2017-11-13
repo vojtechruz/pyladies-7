8b07558ebb8a8f9ee485fcf9f430fe9baa52ded9
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2017-11-13 22:33:18 +0100

    Pridana vyhledavaci tabulka

---
+++ slovniky/slovniky/4-vyhledavaci-tabulka.py
@@ -0,0 +1,16 @@
+#Seznam hodnot stejneho typu, ve kterych se vyhledava podle klice
+#Napr:
+    #Telefonni cisla dle jmena
+    #Cena dle jmena vyrobku
+    #...
+zlateStranky = {
+    'Alice': '603888921',
+    'Bohous':'777891776',
+    'Cyril':'602345666',
+    'Daniela':'728910000',
+    'Eva':'777633798',
+    'Filip':'608915433',
+}
+
+#Jak zjistim jake cilso ma Eva?
+print('Eva ma cislo {}'.format(zlateStranky['Eva']))

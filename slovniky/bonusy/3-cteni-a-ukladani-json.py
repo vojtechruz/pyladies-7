1b2fd606f014f499262180057b21fce7125e6c05
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2017-11-14 14:30:13 +0100

    Pridan JSON bonus ukol

---
+++ slovniky/bonusy/3-cteni-a-ukladani-json.py
@@ -0,0 +1,13 @@
+#Ukol
+# - nacti JSON retezec nize jako Python Slovnik
+# - zmen polozku 'Skladem' na False
+# - pridej polozku 'Dopravne' s hodnotou 199
+# - preved vysledny slovnik na JSON retezec a vypis ho do konzole
+
+jsonRetezecZEshopu = """
+{
+    "JménoProduktu": "Kosmodisk",
+    "Cena": 2999,
+    "Skladem": true,
+}
+"""

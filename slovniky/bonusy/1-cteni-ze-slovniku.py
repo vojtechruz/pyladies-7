7727539281bcd5b47f71c8230cdb14ced65abae1
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2017-11-14 14:21:24 +0100

    Pridany bonusy pro slovniky

---
+++ slovniky/bonusy/1-cteni-ze-slovniku.py
@@ -0,0 +1,12 @@
+#Ukol
+# - Zeptej se uzivatele na jmeno, ktere chce vyhledat. Vrat mu prislusne telefonni cislo
+#Bonus: Pokud zadane jmeno v seznamu neni, informuj uzivatele a ptej se znovu
+
+zlateStranky = {
+    'Alice': '603888921',
+    'Bohous':'777891776',
+    'Cyril':'602345666',
+    'Daniela':'728910000',
+    'Eva':'777633798',
+    'Filip':'608915433',
+}
---
+++ slovniky/bonusy/2-pridavani-do-slovniku.py
@@ -0,0 +1,16 @@
+#Ukol
+# - Zeptej se uzivatele na jmeno ktere chce pridat do seznamu
+# - Pote se zeptej na telefonni cislo
+# - Pridej ziskane polozky do telefoniho seznamu nize
+# Bonus: Zkontroluj ze zadane cislo je cele cislo, jinak se ptej znovu
+# Bonus: Zkntroluj zda dane jmenu v seznamu uz neni. Pokud tam je, ptej se znovu
+
+zlateStranky = {
+    'Alice': '603888921',
+    'Bohous':'777891776',
+    'Cyril':'602345666',
+    'Daniela':'728910000',
+    'Eva':'777633798',
+    'Filip':'608915433',
+}
+

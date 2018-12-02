f81c35bda599cebd8c5fc6ce64fa963ea3bbb7af
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-12-02 11:24:56 +0100

    Pridany priklady k zaverecnemu projektu

---
+++ zaverecny-projekt/snake/6_snake_image_loading.py
@@ -0,0 +1,18 @@
+import pyglet
+
+snake_image = pyglet.image.load('snake.png')
+snake_sprite = pyglet.sprite.Sprite(snake_image)
+
+def draw_snake():
+    # Clear contents of the window, delete everything
+    window.clear()
+    # Draw snake.
+    # The position is lower-left corner if not specified otherwise
+    snake_sprite.draw()
+
+
+window = pyglet.window.Window()
+# Every time we need to redraw (for example when minimalizing and then
+# maximizing the app window), function draw_snake will be called
+window.push_handlers(on_draw=draw_snake)
+pyglet.app.run()

784a8f9d1aa2a8029896089292764d9c926f560a
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2019-03-26 09:55:31 +0100

    Ukoly pro lekci o while

---
+++ while a funkce/while/11_ukol_1.py
@@ -0,0 +1,17 @@
+# Napis program, kdy pocitac bude hazet sestistennou kostkou
+# Tak dlouho dokud nepadne 6
+# Kazdy hod vypise do konzole
+# Kdyz padne 6 uz se dal nehazi a do konzole se vypise "Konec hry!"
+
+# napoveda:
+# from random import randrange
+# randrange(7)
+
+#Napr.
+# 1
+# 3
+# 3
+# 2
+# 5
+# 6
+# Konec hry!

cfa1c2933bfbe174f53e15d7141d243eddcd4994
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2019-05-28 09:02:28 +0200

    Asteroids: priklad mnozin

---
+++ zaverecny-projekt/asteroids/09_sets.py
@@ -0,0 +1,11 @@
+# Mnozina se vytvari pomoci set(['Prvek', 'Druhy prvek']) nebo {'Prvek', 'Druhy prvek'}
+# Prazdna mnozina je set()
+# Mnozina neni serazena
+
+# Kazdy prvek v mnozine muze byt nanejvys jednou
+
+# Lze testovat pritomnost prvku v mnozine
+
+# if(prvek in mnozina)
+# if(prvek not in mnozina)
+

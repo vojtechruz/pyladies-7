9eb69dd579f042e012894669c3cfc411afa9a5a4
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2019-05-28 08:22:45 +0200

    Asteroids: examples for keyboard handling

---
+++ zaverecny-projekt/asteroids/7_asteroids_keyboard_arrows.py
@@ -0,0 +1,25 @@
+import pyglet
+
+def key_pressed(symbol, modifiers):
+
+    # Keys can be accessed via:
+    # pyglet.window.key.*
+    # Such as pyglet.window.key.A
+    # or pyglet.window.key.ENTER
+
+    if(symbol == pyglet.window.key.UP):
+        print("Going UP!")
+    elif(symbol == pyglet.window.key.DOWN):
+        print("Going DOWN")
+    elif(symbol == pyglet.window.key.LEFT):
+        print("Going LEFT")
+    elif(symbol == pyglet.window.key.RIGHT):
+        print("Going RIGHT")
+
+window = pyglet.window.Window()
+# When a user presses a key, key_pressed function will be called
+window.push_handlers(on_key_press=key_pressed)
+pyglet.app.run()
+
+# Documentation of pyglet keyboard handling
+# https://pyglet.readthedocs.io/en/pyglet-1.3-maintenance/programming_guide/keyboard.html

22a07feeffd181e0e7d607fdde746be8a385c995
Jana Mojdlová <jana.mojdlova@seznam.cz>
2019-10-22 19:56:47 +0200

    rozpracováno

--- soubory/procvicovani/BONUS_4_pythagoras.py
+++ soubory/procvicovani/BONUS_4_pythagoras.py
@@ -1,4 +1,17 @@
 # -*- coding: UTF-8 -*-
-# Napis funkci, ktera ti pro dve zadane delky odvesen pravouhleho trojuhelnika vypocita delku prepony. (TIP budes potrebovat Pythagorovu vetu)
+# Napis funkci, ktera ti pro dve zadane delky odvesen pravouhleho 
+# trojuhelnika vypocita delku prepony. 
+# (TIP budes potrebovat Pythagorovu vetu)
 
-# BONUS: Pridej funkce i pro vypocet strany, pokud zname jednu odvesnu a preponu
+# BONUS: Pridej funkce i pro vypocet strany, 
+# pokud zname jednu odvesnu a preponu
+
+# Pythagorova věta: a**2 * b**2 = c**2
+
+def vypocti_delku_prepony(a, b):
+    # a = int(input("Zadej délku první odvěsny "))
+    # b = int(input("Zadej délku druhé odvěsny "))
+    prepona = a**2 * b**2 #odmocnina?
+
+
+vypocti_delku_prepony()

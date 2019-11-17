10e8466d8d40ca3d7f26f3f75c1fe2eebf134052
Jana Mojdlová <jana.mojdlova@seznam.cz>
2019-11-17 18:11:31 +0100

    vypracovano

--- testovani/bonus/1_import_factorial.py
+++ testovani/bonus/1_import_factorial.py
@@ -5,3 +5,21 @@
 #Rozsirujici zadani:
 # - Osetri vstup tak, aby uzivatel mohl zadat pouze cele cislo
 # - Osetri vstup tak aby uzivatel nemohl zadat zaporne cislo
+
+from math import factorial
+
+while True:
+    odpoved = input('Zadej celé kladné číslo ')
+    try:
+        cislo = int(odpoved)
+    except ValueError:
+        print('Toto není celé číslo! Zkus to znovu.')
+    else:
+        if int(odpoved) < 0:
+            print('Toto není kladné číslo! Zkus to znovu.')
+        else:
+            break
+
+faktorial_cisla = factorial(cislo)
+print('Faktorial čísla {0} je {1}.'.format(cislo, faktorial_cisla))
+

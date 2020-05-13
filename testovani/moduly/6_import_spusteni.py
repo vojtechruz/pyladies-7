577d13a0f2d93c3f25b03e59f5a2b698c3838446
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2020-05-13 17:05:37 +0200

    Pridan priklad s vyhonanim kodu v importovanem modulu

---
+++ testovani/moduly/6_import_spusteni.py
@@ -0,0 +1,4 @@
+from import_spusteni import kolik_je_hodin
+
+print("Ted ti reknu kolik je hodin:")
+print(kolik_je_hodin())
---
+++ testovani/moduly/import_spusteni.py
@@ -0,0 +1,4 @@
+def kolik_je_hodin():
+    return "Je okolo pate"
+
+print("Presny cas: ", kolik_je_hodin())

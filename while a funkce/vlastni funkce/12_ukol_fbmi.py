a6aec939be04e048ea260c3a88737f28e2be51a8
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2020-10-14 17:14:14 +0200

    Pridan druhy ukol pro vlastni funkce

---
+++ while a funkce/vlastni funkce/12_ukol_fbmi.py
@@ -0,0 +1,16 @@
+# Vytvor funkci, ktera spocita Body Mass Index pro kocky
+
+# - vstup: Obvod hrudniku (cm), delka zadni nohy od kolena ke kotniku (cm)
+# - vystup - cislo, feline body mass index, ktery se spocita takto:
+
+# spocitani indexu:
+# Vydelit obvod hrudniku 0.7062 a odecist delku nohy
+# Vydelit vysledek 0.9156
+# Od toho odecist delku nohy
+
+# Napis dalsi funkci, ktera ti na zaklade FBMI vrati jak na tom kocka je:
+
+# < 15 Podvyziva
+# 15-30 V Norme
+# 30-42 Nadvaha
+# 42+ Tezka Obezita

00613e2ff1bea2b8b8f60d3e4ce7a83345f2300d
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2020-10-14 17:32:46 +0200

    Vlastni funkce - drobne upravy ukazek kodu

--- while a funkce/vlastni funkce/01_funkce.py
+++ while a funkce/vlastni funkce/01_funkce.py
@@ -7,3 +7,6 @@
 # Mohou vracet nejakou hodnotu, s kterou pak muzeme dale pracovat
 barva = input("Zadej svou oblibenou barvu\n")
 print("Tva oblibena barva je " + barva)
+
+# Tahak s uzitecnymi existujicimi funkcemi
+# https://pyvec.github.io/cheatsheets/basic-functions/basic-functions-cs.pdf
--- while a funkce/vlastni funkce/04_vraceni_hodnoty.py
+++ while a funkce/vlastni funkce/04_vraceni_hodnoty.py
@@ -7,6 +7,10 @@ def obvod_obdelniku(sirka, vyska):
     "Vypocita obvod obdelniku dane sirky a vysky"
     return 2 * (sirka + vyska)
 
+def obvod_kvadru(a,b,c):
+    return 2*obvod_obdelniku(a,b) + 4*c
+
 obvod_obdelniku_stary(1, 1)
 print(obvod_obdelniku(5, 10))
 print(obvod_obdelniku(2, 7)*2)
+print(obvod_kvadru(10,5,20))

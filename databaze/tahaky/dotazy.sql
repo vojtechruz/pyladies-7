ba737c8a02ecbf8759aa0ce05475cb46a18aacda
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2024-05-27 16:36:17 +0200

    DB - pridany nove dotazy

--- databaze/tahaky/dotazy.sql
+++ databaze/tahaky/dotazy.sql
@@ -8,3 +8,6 @@ select fact from cat_facts;
 -- Podminka - pouze radky ktere maji source s urcitou hodnotou
 select fact, source from cat_facts where source = 'api';
 select fact, source from cat_facts where source = 'user';
+
+-- Vraci i automaticky generovany identifikator radku (ktery se generuje pokud neni primarni klic explicitne definovan)
+select rowid, fact from cat_facts
--- databaze/tahaky/tabulky.sql
+++ databaze/tahaky/tabulky.sql
@@ -8,5 +8,8 @@ create table if not exists cat_facts (fact varchar20, source varchar20);
 -- Pro jednotlive sloupce muzeme nastavit ze hodnota nesmi byt prazdna
 create table cat_facts (fact varchar20 not null, source varchar20 not null);
 
+-- Vytvori tabulku s id jako primarnim klicem, pokud by nebyl definovan SQLite automaticky vytvori sloupec ROWID jako primarni klic a pro kazdou polozku inkrementuje jeho hodnotu
+create table cat_facts (id not null primary key,  fact varchar20, source varchar20);
+
 -- Smaze celou tabulku
 drop table cat_facts;
